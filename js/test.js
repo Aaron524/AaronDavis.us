de8d6ce92e98c33e946231c04c8998b632faaeb7
Aaron Davis <ajdavis425@gmail.com>
2016-04-15 16:20:41 -0400

    V3 of homepage
    Started working on adding images and new buttons

--- js/test.js
+++ js/test.js
@@ -1,3 +1,40 @@
+var cows = 7;
+var chickens = 11;
+var targetFarm = cows + chickens;
+
+printFarmInventory(cows, chickens);
+console.log(findSolution(targetFarm));
+
+function findSolution(target) {
+  function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
+
+
+
+function zeroPad(number, width) {
+  var string = String(number);
+  while (string.length < width)
+    string = "0" + string;
+  return string;
+}
+
+function printFarmInventory(cows, chickens, pigs) {
+  console.log(zeroPad(cows, 3) + " Cows");
+  console.log(zeroPad(chickens, 3) + " Chickens");
+  console.log(zeroPad(pigs, 3) + " Pigs");
+}
+
+printFarmInventory(7, 16, 3);
+
 function count_rabbits() {
     for(var i=1; i<=3; i++) {
         // operator + concatenates strings
