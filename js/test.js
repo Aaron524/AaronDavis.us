3e98c2d5db1128bd5845ed6d1365903839248a59
Aaron Davis <ajdavis425@gmail.com>
2016-05-11 15:31:58 -0400

    JQuery add

--- js/test.js
+++ js/test.js
@@ -306,3 +306,21 @@ $(document).ready(function() {
 
 
 //adding and removing a class based on event
+
+
+
+
+//animating examples
+
+$(document).ready(function() {
+  $('.tour').on('mouseenter', function() {
+    $(this).addClass('highlight');
+    $(this).find('.per-night').animate({'top': '-14px', 'opacity': '1'}, 'fast');
+  });
+  $('.tour').on('mouseleave', function() {
+    $(this).removeClass('highlight');
+    $(this).find('.per-night').animate({'top': '0px', 'opacity': '0'}, 'fast');    
+  });
+});
+
+//animating examples
